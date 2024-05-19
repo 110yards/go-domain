8d22ebf54716b83edacb44ad96126cbf3daba9e1
mjdryden <mjdryden@gmail.com>
2024-05-19 01:49:44 +0000

    ScoreboardGame -> GameResult

--- game_result.go
+++ game_result.go
@@ -2,19 +2,25 @@ package domain
 
 import "time"
 
-type ScoreboardGame struct {
+type GameWinner string
+
+const (
+	NoResult       GameWinner = "no_result"
+	AwayGameWinner GameWinner = "away"
+	HomeGameWinner GameWinner = "home"
+	TieGameWinner  GameWinner = "tie"
+)
+
+type GameResult struct {
 	GameId     string     `json:"game_id"`
 	DateStart  time.Time  `json:"date_start"`
 	GameNumber int        `json:"game_number"`
+	WeekNumber int        `json:"week_number"`
 	GameType   GameType   `json:"game_type"`
 	GameStatus GameStatus `json:"game_status"`
 	Away       Team       `json:"away"`
 	Home       Team       `json:"home"`
 	AwayScore  int        `json:"away_score"`
 	HomeScore  int        `json:"home_score"`
-}
-
-type Scoreboard struct {
-	Games      []ScoreboardGame `json:"games"`
-	WeekNumber int              `json:"week_number"`
+	Winner     GameWinner `json:"winner"`
 }
