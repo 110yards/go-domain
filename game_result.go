57b3da11a96c67237e32d322eb7149bdd1bd386f
mjdryden <mjdryden@gmail.com>
2024-05-19 02:09:10 +0000

    Add date updated to gameresult

--- game_result.go
+++ game_result.go
@@ -12,16 +12,17 @@ const (
 )
 
 type GameResult struct {
-	GameId     string     `json:"game_id"`
-	Year       int        `json:"year"`
-	DateStart  time.Time  `json:"date_start"`
-	GameNumber int        `json:"game_number"`
-	WeekNumber int        `json:"week_number"`
-	GameType   GameType   `json:"game_type"`
-	GameStatus GameStatus `json:"game_status"`
-	Away       Team       `json:"away"`
-	Home       Team       `json:"home"`
-	AwayScore  int        `json:"away_score"`
-	HomeScore  int        `json:"home_score"`
-	Winner     GameWinner `json:"winner"`
+	GameId      string     `json:"game_id"`
+	Year        int        `json:"year"`
+	DateStart   time.Time  `json:"date_start"`
+	GameNumber  int        `json:"game_number"`
+	WeekNumber  int        `json:"week_number"`
+	GameType    GameType   `json:"game_type"`
+	GameStatus  GameStatus `json:"game_status"`
+	Away        Team       `json:"away"`
+	Home        Team       `json:"home"`
+	AwayScore   int        `json:"away_score"`
+	HomeScore   int        `json:"home_score"`
+	Winner      GameWinner `json:"winner"`
+	DateUpdated time.Time  `json:"date_updated"`
 }
